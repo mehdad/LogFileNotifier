cc3f14148f6c0b1b26693880d1a137cf553e0061
mehrdad <m.mohamadi313@gmail.com>
2022-01-19 15:07:03 +0330

    Change file size to line count

--- notify.bash
+++ notify.bash
@@ -5,19 +5,19 @@ command_name=`basename "$0"`
 url="$BASE_URL/$WORKSPACE/arian/notif/admin-notifications"
 file="$BASE_PATH/shared/sites/$WORKSPACE/log/"
 config_path="$BASE_PATH/workflow/engine/config/env.ini"
-maxsize=$(awk -v RS='\r\n' -F "=" '/max_log_file_size/ {print $2}' $config_path)
 
 while inotifywait -r -e close_write "$file"; do
-      actualsize=$(du -sh -k "$file" | cut -f1)
+      maxsize=$(awk -v RS='\r\n' -F "=" '/max_log_file_size/ {print $2}' $config_path)
+      actualsize=$(find $file -type f -name "audit*" -exec cat {} + | wc -l)
       diff=$(( ${actualsize} - ${maxsize}))
       echo "actualsize=$actualsize" 
       echo "maxsize=$maxsize" 
       echo "diff=$diff"         
       if [ ${diff} -ge 0 ]; then
-        echo size is over $maxsize kilobytes
+        echo size is over $maxsize lines
         curl --location --request POST "$url" --header "$TOKEN"
     else
-        echo size is under $maxsize kilobytes
+        echo size is under $maxsize lines
     fi
 done
 ./$command_name "$WORKSPACE" "$file"
